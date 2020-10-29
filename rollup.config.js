23baa7780777de1c2b35baf1c990e38dd5fef41c
罗国雄 <1025558554@qq.com>
2020-10-29 11:36:27 +0800

    close all

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,6 @@
-import typescript from 'rollup-plugin-typescript';
-import sourceMaps from 'rollup-plugin-sourcemaps';
-
 export default {
   input: './src/index.js',
-  plugins: [
-    typescript({
-      exclude: 'node_modules/**',
-      typescript: require('typescript'),
-    }),
-    sourceMaps(),
-  ],
+  plugins: [],
   output: [
     {
       format: 'cjs',
